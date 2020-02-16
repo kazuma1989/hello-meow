3c18eff54f4fc03c30bcec6e1206544734d47490
kazuma1989 <kazuma.ebina@quipper.com>
2020-02-16 14:02:17 +0900

    chore: Install meow

---
+++ index.js
@@ -0,0 +1,33 @@
+// @ts-check
+const meow = require("meow");
+
+const cli = meow(
+  `
+  Usage
+    $ hello-meow <input> [...<input>]
+
+  Options
+    --rainbow, -r  Include a rainbow
+
+  Examples
+    $ hello-meow unicorns --rainbow
+    🌈 unicorns 🌈
+`,
+  {
+    flags: {
+      rainbow: {
+        type: "boolean",
+        default: false,
+        alias: "r"
+      }
+    }
+  }
+);
+
+const {
+  input,
+  flags: { rainbow }
+} = cli;
+
+const output = input.join(" ");
+console.log(rainbow ? `🌈 ${output} 🌈` : output);
