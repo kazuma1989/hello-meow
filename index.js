020557c2bb2188918073bbf940307c8c879011b3
kazuma1989 <kazuma.ebina@quipper.com>
2020-02-19 21:06:41 +0900

    feat: サブコマンドに対応してみた

--- index.js
+++ index.js
@@ -1,34 +1,96 @@
-#!/user/bin/env node
 // @ts-check
 const meow = require("meow");
 
-const cli = meow(
-  `
-  Usage
-    $ hello-meow <input> [...<input>]
+const cli = (helpText, options = {}) => {
+  const result = meow(
+    helpText ||
+      `
+    Usage
+      $ hello-meow [...INPUT]
 
-  Options
-    --rainbow, -r  Include a rainbow
+    Options
+      --verbose, -v  Verbose output
+      --rainbow, -r  Include a rainbow
 
-  Examples
-    $ hello-meow unicorns --rainbow
-    🌈 unicorns 🌈
+    Examples
+      $ hello-meow -v unicorns --rainbow
+      🌈 unicorns 🌈
 `,
-  {
-    flags: {
-      rainbow: {
-        type: "boolean",
-        default: false,
-        alias: "r"
+    {
+      ...options,
+      flags: {
+        verbose: {
+          type: "boolean",
+          default: false,
+          alias: "v"
+        },
+        rainbow: {
+          type: "boolean",
+          default: false,
+          alias: "r"
+        },
+        ...options.flags
       }
     }
+  );
+
+  if (helpText) {
+    const {
+      input: [, ...input],
+      flags: { help },
+      showHelp
+    } = result;
+    if (!input.length && help) {
+      showHelp();
+    }
   }
-);
 
-const {
-  input,
-  flags: { rainbow }
-} = cli;
+  return result;
+};
 
-const output = input.join(" ");
-console.log(rainbow ? `🌈 ${output} 🌈` : output);
+const subCommand = {
+  double(parent) {
+    const {
+      input: [, ...input],
+      flags
+    } = parent(
+      `
+      Usage
+        $ hello-meow double [...INPUT]
+
+      Options
+        --rainbow, -r  Include a doubled rainbow
+
+      Examples
+        $ hello-meow double unicorns --rainbow
+        🌈🌈 unicorns 🌈🌈
+`,
+      {
+        flags: {
+          // Override a flag
+          rainbow: {
+            type: "string",
+            default: "",
+            alias: "r"
+          }
+        }
+      }
+    );
+
+    console.log("sub", input, flags);
+    // const output = input.join(" ");
+    // console.log(rainbow ? `🌈 ${output} 🌈` : output);
+  }
+};
+
+const base = cli();
+
+const [command] = base.input;
+if (subCommand[command]) {
+  subCommand[command](cli);
+} else {
+  console.log(base.input, base.flags);
+}
+
+// const output = input.join(" ");
+// console.log(rainbow ? `🌈 ${output} 🌈` : output);
